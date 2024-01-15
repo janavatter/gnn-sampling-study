ea675f6328019ec16d452d64b6be2bf21cb6c72e
Jana Vatter <33870049+janavatter@users.noreply.github.com>
2024-01-15 07:39:38 -0500

    update layerwise_inference.py

--- code/layerwise_inference.py
+++ code/layerwise_inference.py
@@ -2,11 +2,19 @@ import torch
 import torchmetrics.functional as MF
 
 
-def layerwise_infer(device, graph, nid, model, out_size, batch_size):
+def layerwise_infer(device, graph, nid, model, dataset, batch_size, out_size):
     model.eval()
     with torch.no_grad():
-        pred = model.inference(graph, device, batch_size)
+        pred = model.inference(graph, device, batch_size) # pred in buffer_device
         pred = pred[nid]
         label = graph.ndata['label'][nid].to(pred.device)
+        #return MF.accuracy(pred, label)
 
-        return MF.accuracy(pred, label, task='multiclass', num_classes=int(out_size))
+        if dataset == 'yelp':
+            acc = MF.classification.multilabel_accuracy(pred, label, num_labels=int(out_size))
+            f1 = MF.classification.multilabel_f1_score(pred, label, num_labels=int(out_size))
+        else:
+            acc = MF.accuracy(pred, label, task='multiclass', num_classes=int(out_size))
+            f1 = MF.f1_score(pred, label, task='multiclass', num_classes=int(out_size))
+
+    return acc, f1
