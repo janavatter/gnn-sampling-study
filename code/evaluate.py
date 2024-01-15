f24da9f68ac18bdba4c9545c22ed7776494e52d5
Jana Vatter <33870049+janavatter@users.noreply.github.com>
2024-01-15 07:36:55 -0500

    adapted eval to Yelp graph

--- code/evaluate.py
+++ code/evaluate.py
@@ -2,7 +2,7 @@ import torch
 import torchmetrics.functional as MF
 
 
-def evaluate_blocks(model, graph, dataloader, out_size):
+def evaluate_blocks(model, graph, dataloader, out_size, dataset):
     model.eval()
     ys = []
     y_hats = []
@@ -11,10 +11,18 @@ def evaluate_blocks(model, graph, dataloader, out_size):
             x = blocks[0].srcdata['feat']
             ys.append(blocks[-1].dstdata['label'])
             y_hats.append(model(blocks, x))
-    return MF.accuracy(torch.cat(y_hats), torch.cat(ys), task='multiclass', num_classes=int(out_size))
+            
+    if dataset == 'yelp':
+        acc = MF.classification.multilabel_accuracy(torch.cat(y_hats), torch.cat(ys), num_labels=int(out_size))
+        f1 = MF.classification.multilabel_f1_score(torch.cat(y_hats), torch.cat(ys), num_labels=int(out_size))
+    else:
+        acc = MF.accuracy(torch.cat(y_hats), torch.cat(ys), task='multiclass', num_classes=int(out_size))
+        f1 = MF.f1_score(torch.cat(y_hats), torch.cat(ys), task='multiclass', num_classes=int(out_size))
+
+    return acc, f1
 
 
-def evaluate_subgraph_shadow(model, graph, dataloader, out_size):
+def evaluate_subgraph_shadow(model, graph, dataloader, out_size, dataset):
     model.eval()
     ys = []
     y_hats = []
@@ -23,10 +31,18 @@ def evaluate_subgraph_shadow(model, graph, dataloader, out_size):
             x = blocks.ndata['feat']
             ys.append(blocks.ndata['label'])
             y_hats.append(model(blocks, x))
-    return MF.accuracy(torch.cat(y_hats), torch.cat(ys), task='multiclass', num_classes=int(out_size))
+            
+    if dataset == 'yelp':
+        acc = MF.classification.multilabel_accuracy(torch.cat(y_hats), torch.cat(ys), num_labels=int(out_size))
+        f1 = MF.f1_score(torch.cat(y_hats), torch.cat(ys), task='multilabel', num_labels=int(out_size))
+    else:
+        acc = MF.accuracy(torch.cat(y_hats), torch.cat(ys), task='multiclass', num_classes=int(out_size))
+        f1 = MF.f1_score(torch.cat(y_hats), torch.cat(ys), task='multiclass', num_classes=int(out_size))
+
+    return acc, f1
 
 
-def evaluate_subgraph(model, graph, dataloader, out_size):
+def evaluate_subgraph(model, graph, dataloader, out_size, dataset):
     model.eval()
     ys = []
     y_hats = []
@@ -35,13 +51,28 @@ def evaluate_subgraph(model, graph, dataloader, out_size):
             x = subgraph.ndata['feat']
             ys.append(subgraph.ndata['label'])
             y_hats.append(model(subgraph, x))
-    return MF.accuracy(torch.cat(y_hats), torch.cat(ys), task='multiclass', num_classes=int(out_size))
+            
+    if dataset == 'yelp':
+        acc = MF.classification.multilabel_accuracy(torch.cat(y_hats), torch.cat(ys), num_labels=int(out_size))
+        f1 = MF.f1_score(torch.cat(y_hats), torch.cat(ys), task='multilabel', num_labels=int(out_size))
+    else:
+        acc = MF.accuracy(torch.cat(y_hats), torch.cat(ys), task='multiclass', num_classes=int(out_size))
+        f1 = MF.f1_score(torch.cat(y_hats), torch.cat(ys), task='multiclass', num_classes=int(out_size))
+
+    return acc, f1
 
 
-def evaluate_full(g, features, labels, mask, model, out_size):
+def evaluate_full(g, features, labels, mask, model, out_size, dataset):
     model.eval()
     with torch.no_grad():
         logits = model(g, features)
         logits = logits[mask]
         labels = labels[mask]
-        return MF.accuracy(logits, labels, task='multiclass', num_classes=int(out_size))
+        if dataset == 'yelp':
+            acc = MF.classification.multilabel_accuracy(logits, labels, num_labels=int(out_size))
+            f1 = MF.f1_score(logits, labels, task='multilabel', num_labels=int(out_size))
+        else:
+            acc = MF.accuracy(logits, labels, task='multiclass', num_classes=int(out_size))
+            f1 = MF.f1_score(logits, labels, task='multiclass', num_classes=int(out_size))
+
+        return acc, f1
